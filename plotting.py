f67f052e2858e3e65ecfcd383b102c6116c512d5
Daniel Klein <daniel.klein@gatesfoundation.org>
2024-03-28 00:03:07 -0700

    Improvements to SIR and plotting.

--- plotting.py
+++ plotting.py
@@ -3,14 +3,30 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import networkx as nx
+import sciris as sc
+import datetime as dt
 
-def plot_scenarios(df, figdir, channels=None):
-    sns.set(font_scale=1.4, style='whitegrid')
+def fix_dates(g):
+    for ax in g.axes.flat:
+        #ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+        #ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter())
+        #ax.xaxis.set_major_locator(mdates.MonthLocator())
+        locator = mdates.AutoDateLocator(minticks=3, maxticks=7)
+        formatter = mdates.ConciseDateFormatter(locator)
+        ax.xaxis.set_major_locator(locator)
+        ax.xaxis.set_major_formatter(formatter)
+
+def plot_scenarios(df, figdir, channels=None, var1='cov', var2='channel', slice_year=-1):
+    sns.set(font_scale=1.2, style='whitegrid')
 
     # Renaming
     df.replace({'rng': {'centralized':'Centralized', 'multi': 'CRN'}}, inplace=True)
     rngs = ['Centralized', 'CRN']
+
+    assert df['year'].iloc[0] == 2020
+    df['date'] = pd.to_datetime(365 * (df['year']-2020), unit='D', origin=dt.datetime(year=2020, month=1, day=1)) #pd.to_datetime(df['year'], format='%f', unit='Y')
 
     covs = df['cov'].unique()
     covs.sort()
@@ -24,99 +40,150 @@ def plot_scenarios(df, figdir, channels=None):
     df.rename(columns={'cov': 'Coverage'}, inplace=True)
     cov = 'Coverage'
 
-    d = pd.melt(df, id_vars=['year', 'rand_seed', cov, 'rng', 'network'], var_name='channel', value_name='Value')
+    id_vars = ['date', 'rand_seed', cov, 'rng', 'network', 'eff', 'n_agents']
+    d = pd.melt(df, id_vars=['date', 'rand_seed', cov, 'rng', 'network', 'eff', 'n_agents'], var_name='channel', value_name='Value')
+
+    # Slice to the one channel specified by the user
+    d = d.loc[d['channel'].isin(channels)]
+
+    if var1 != 'channel' and var2 != 'channel':
+        assert len(channels)==1, 'If not slicing over channel, only one channel can be provided.'
+
     d['baseline'] = d[cov]=='Reference'
     bl = d.loc[d['baseline']]
     scn = d.loc[~d['baseline']]
-    bl = bl.set_index(['year', 'channel', 'rand_seed', cov, 'rng', 'network'])[['Value']].reset_index(cov)
-    scn = scn.set_index(['year', 'channel', 'rand_seed', cov, 'rng', 'network'])[['Value']].reset_index(cov)
-    mrg = scn.merge(bl, on=['year', 'channel', 'rand_seed', 'rng', 'network'], suffixes=('', '_ref'))
+
+    id_vars.append('channel')
+    bl = bl.set_index(id_vars)[['Value']].reset_index(cov)
+    scn = scn.set_index(id_vars)[['Value']].reset_index(cov)
+
+    id_vars.remove(cov)
+    mrg = scn.merge(bl, on=id_vars, suffixes=('', '_ref'))
     mrg['Value - Reference'] = mrg['Value'] - mrg['Value_ref']
     mrg = mrg.sort_index()
     mrg['Coverage'] = pd.Categorical(mrg['Coverage'], categories=cov_ord[:-1]) # Agh^2!
 
-    cor = mrg.groupby(['year', 'channel', 'rng', cov])[['Value', 'Value_ref']].apply(lambda x: np.corrcoef(x['Value'], x['Value_ref'], rowvar=False)[0,1])
+    id_vars = ['date', cov, 'rng', 'network', 'eff', 'n_agents', 'channel']
+    cor = mrg.groupby(id_vars)[['Value', 'Value_ref']].apply(lambda x: np.corrcoef(x['Value'], x['Value_ref'], rowvar=False)[0,1])
     cor.name = 'Pearson'
     #cor.replace([np.inf, -np.inf, np.nan], 1, inplace=True)
 
     kw = {'height': 3, 'aspect': 1.4}
     fkw = {'sharey': False, 'sharex': 'col', 'margin_titles': True}
 
-    # Make a color palette for timeseries
-    Set1_mod = sns.color_palette('Set1') 
-    #Set1_mod = [Set1_mod[2]] + [Set1_mod[0]] + [Set1_mod[1]]
-    #print(Set1_mod)
-    first = Set1_mod.pop(0) # Move the 1st color to len(cov) for consistency
-    n = len(covs)-1
-    Set1_mod = Set1_mod[:n] + [first]
-    #print(Set1_mod)
+    var1_ord = None
+    if var1=='cov':
+        var1 = cov
+        var1_ord = None # Categorical, so not needed
 
-    ## TIMESERIES
-    g = sns.relplot(kind='line', data=d, x='year', y='Value', hue=cov, hue_order=cov_ord, col='channel', col_order=channels, row='rng', row_order=rngs,
+    if var2=='cov':
+        var2 = cov
+        var2_ord = None # Categorical, so not needed
+
+    rng_colors = [(0,1,1), (1,0,1)]
+
+    
+    #%% TIMESERIES
+    g = sns.relplot(kind='line', data=d, x='date', y='Value', hue=var1, hue_order=var1_ord, row=var2, col='rng', col_order=rngs,
         palette='Set1', errorbar='sd', lw=2, facet_kws=fkw, **kw)
     g.set_titles(col_template='{col_name}', row_template='{row_name}')
-    g.set_xlabels('Year')
+    g.set_xlabels('Date')
+    fix_dates(g)
     g.figure.savefig(os.path.join(figdir, 'timeseries.png'), bbox_inches='tight', dpi=300)
 
-    ## TIMESERIES: specific channels
+
+    #%% TIMESERIES: specific channels
     for ch in ['Maternal Deaths', 'Infected']:
         if ch in d['channel'].unique():
-            g = sns.relplot(kind='line', data=d, x='year', y='Value', hue=cov, hue_order=cov_ord, row='channel', row_order=[ch], col='rng', col_order=rngs,
-                palette=Set1_mod, errorbar='sd', lw=2, facet_kws=fkw, **kw)
+            g = sns.relplot(kind='line', data=d, x='date', y='Value', hue=var1, hue_order=var1_ord, row='channel', row_order=[ch], col='rng', col_order=rngs,
+                palette='Set1', errorbar='sd', lw=2, facet_kws=fkw, **kw)
             g.set_titles(col_template='{col_name}', row_template='{row_name}')
-            g.set_xlabels('Year')
+            g.set_xlabels('Date')
+            fix_dates(g)
             g.figure.savefig(os.path.join(figdir, f'timeseries_{ch.replace(" ", "")}.png'), bbox_inches='tight', dpi=300)
 
-    ## DIFF TIMESERIES
+
+    #%% DIFF TIMESERIES
     for ms, mrg_by_ms in mrg.groupby('rng'):
-        g = sns.relplot(kind='line', data=mrg_by_ms, x='year', y='Value - Reference', hue=cov, col='channel', col_order=channels,
-                palette='Set1', estimator=None, units='rand_seed', lw=0.5, facet_kws=fkw, **kw) # row=cov, 
+        g = sns.relplot(kind='line', data=mrg_by_ms, x='date', y='Value - Reference', hue=var1, hue_order=var1_ord, col=var2, #col_order=var2_ord,
+                palette='Set1', estimator=None, units='rand_seed', lw=0.5, facet_kws=fkw, **kw)
         g.set_titles(col_template='{col_name}', row_template='{row_name}')
         #g.figure.suptitle('MultiRNG' if ms else 'SingleRNG')
         g.figure.subplots_adjust(top=0.88)
-        g.set_xlabels('Year')
+        g.set_xlabels('Date')
+        fix_dates(g)
         g.figure.savefig(os.path.join(figdir, f'diff_{ms}.png'), bbox_inches='tight', dpi=300)
 
-    ## FINAL TIME
-    tf = df['year'].max()
-    mtf = mrg.loc[tf]
-    g = sns.displot(data=mtf.reset_index(), kind='kde', fill=True, rug=True, cut=0, hue=cov, x='Value - Reference',
-            col='channel', col_order=channels, row='rng', row_order=rngs, facet_kws=fkw, palette='Set1', **kw)
-    g.set_titles(col_template='{col_name}', row_template='{row_name}')
-    g.set_xlabels(f'Value - Reference at year {tf}')
-    g.figure.savefig(os.path.join(figdir, 'final.png'), bbox_inches='tight', dpi=300)
 
-    ## COR SCATTER FINAL TIME
-    ctf = mtf.reset_index('rand_seed').set_index(cov, append=True).sort_index()
-    g = sns.relplot(data=ctf, kind='scatter', hue='rng', hue_order=rngs, style='rng', style_order=rngs, x='Value_ref', y='Value',
-            col='channel', col_order=channels, row=cov, facet_kws=fkw, palette='tab10', **kw)
-    g.set_titles(col_template='{col_name}', row_template='Coverage: {row_name}')
-    g.set_xlabels(f'Reference at year {tf}')
-    g.set_ylabels(f'Value at year {tf}')
-    g.figure.savefig(os.path.join(figdir, 'cor_final.png'), bbox_inches='tight', dpi=300)
+    #%% SLICE AT SPECIFIC TIME
+    if slice_year < 0:
+        slice_year = d['date'].max()
+    else:
+        years = df['year'].unique()
+        dates = d['date'].unique()
+        sidx = sc.findnearest(years, slice_year)
+        slice_year = dates[sidx]
+
+    slice_str = slice_year.strftime('%b %d, %Y')
+
+    mtf = mrg.loc[slice_year]
+    g = sns.displot(data=mtf.reset_index(), kind='kde', fill=True, rug=True, cut=0, hue=var1, hue_order=var1_ord, x='Value - Reference',
+            row=var2, col='rng', col_order=rngs, facet_kws=fkw, palette='Set1', **kw)
+    g.set_titles(col_template='{col_name}', row_template='{row_name}')
+    g.set_xlabels(f'Value - Reference on {slice_str}')
+    g.figure.savefig(os.path.join(figdir, 'slice.png'), bbox_inches='tight', dpi=300)
+
+
+    #%% COR SCATTER AT SLICE TIME
+    ctf = mtf.reset_index('rand_seed')
+    if var1 in ctf.columns:
+        ctf.set_index(var1, append=True)
+    ctf = ctf.sort_index()
+    #if len(channels) > 1:
+    #    g = sns.relplot(data=ctf, kind='scatter', hue='rng', hue_order=rngs, style='rng', style_order=rngs, x='Value_ref', y='Value', size='rng', sizes={'Centralized':10, 'CRN': 10},
+    #        col='channel', col_order=channels, row=var1, row_order=var1_ord, facet_kws=fkw, palette=rng_colors, **kw)
+    #else:
+    facet_kws = fkw.copy()
+    facet_kws['sharey'] = 'row'
+    facet_kws['sharex'] = 'row'
+
+    g = sns.relplot(data=ctf, kind='scatter', hue='rng', hue_order=rngs, x='Value_ref', y='Value', size='rng', sizes={'Centralized':25, 'CRN': 25},
+        style='rng', style_order=rngs, markers={'Centralized':'+', 'CRN': 'x'},
+        col=var1, col_order=var1_ord, row=var2, facet_kws=facet_kws, palette=rng_colors, **kw, lw=None)
+
+    g.set_titles(col_template='{col_name}', row_template='{row_name}')
+    g.set_xlabels(f'Reference on {slice_str}')
+    g.set_ylabels(f'Value on {slice_str}')
+    g.figure.savefig(os.path.join(figdir, 'cor_slice.png'), bbox_inches='tight', dpi=300)
 
     # Share y from here on
     fkw['sharey'] = 'row'
 
-    ## DIFF TIMESERIES ALL IN ONE
-    g = sns.relplot(kind='line', data=mrg, x='year', y='Value - Reference', hue=cov, row='channel', row_order=channels,
+
+    #%% DIFF TIMESERIES ALL IN ONE
+    g = sns.relplot(kind='line', data=mrg, x='date', y='Value - Reference', hue=var1, hue_order=var1_ord, row=var2, #row_order=channels,
             col='rng', col_order=rngs, palette='Set1', estimator=None, units='rand_seed', lw=0.5, facet_kws=fkw, **kw)
     g.set_titles(col_template='{col_name}', row_template='{row_name}')
     g.figure.subplots_adjust(top=0.88)
-    g.set_xlabels('Year')
+    g.set_xlabels('Date')
+    fix_dates(g)
     g.figure.savefig(os.path.join(figdir, f'diff.png'), bbox_inches='tight', dpi=300)
 
 
-    ## CORRELATION OVER TIME
+    #%% CORRELATION OVER TIME
     try:
-        g = sns.relplot(kind='line', data=cor.to_frame(), x='year', y='Pearson', hue='rng', hue_order=rngs, col='channel', col_order=channels,
-                row=cov, palette='tab10', errorbar='sd', lw=2, facet_kws=fkw, **kw)
-        g.set_titles(col_template='{col_name}', row_template='Coverage: {row_name}')
+        corf = cor.to_frame()
+        g = sns.relplot(kind='line', data=corf, x='date', y='Pearson', hue='rng', hue_order=rngs,
+                col=var1, col_order=var1_ord, row=var2,
+                palette=rng_colors, errorbar='sd', lw=2, facet_kws=fkw, **kw)
+        g.set_titles(col_template='{col_name}', row_template='{row_name}')
         g.figure.subplots_adjust(top=0.88)
-        g.set_xlabels('Year')
+        g.set_xlabels('Date')
+        fix_dates(g)
         g.figure.savefig(os.path.join(figdir, 'cor.png'), bbox_inches='tight', dpi=300)
-    except:
+    except Exception as e:
         print('CORRELATION OVER TIME did not work')
+        print(e)
 
     print('Figures saved to:', os.path.join(os.getcwd(), figdir))
 
--- run_SIRstatic.py
+++ run_SIRstatic.py
@@ -24,36 +24,33 @@ warnings.filterwarnings("ignore", "use_inf_as_na")
 
 rngs = ['centralized', 'multi'] # 'single', 
 
-n = 1_000 # Agents
-n_rand_seeds = 10
-intv_cov_levels = [0.01, 0.10, 0.25, 0.90] + [0] # Must include 0 as that's the baseline
+debug = False
+default_n_agents = [10_000, 1_000][debug]
+default_n_rand_seeds = [250, 25][debug]
 
-figdir = os.path.join(os.getcwd(), 'figs', 'SIR')
-sc.path(figdir).mkdir(parents=True, exist_ok=True)
+basedir = os.path.join(os.getcwd(), 'figs')
 
-def run_sim(n, idx, intv_cov, rand_seed, rng, network=None):
+def run_sim(n_agents, idx, cov, rand_seed, rng, network=None, eff=0.8, fixed_initial_prevalence=False):
 
-    ppl = ss.People(n)
+    ppl = ss.People(n_agents)
     # watts_strogatz_graph, erdos_renyi_graph, grid_2d_graph, configuration_model, line_graph, barabasi_albert_graph, scale_free_graph, complete_graph, maybe a tree
-    #G = nx.gnp_random_graph(n, p=3/n, seed=rand_seed) #G=nx.erdos_renyi_graph(n, p=3/n, seed=rand_seed)
-    # G=nx.complete_graph(n)
     if network is None:
-        G=nx.barabasi_albert_graph(n=n, m=1, seed=rand_seed)
+        G=nx.barabasi_albert_graph(n=n_agents, m=1, seed=rand_seed)
         G.name = 'Barabasi-Albert'
     elif callable(network):
-        G = network(n, rand_seed) # Call with n and random seed if callable
+        G = network(n_agents, rand_seed) # Call with n and random seed if callable
     elif isinstance(network, nx.Graph):
         G = network.copy()
     else:
         print('Check the network parameter')
 
-    lbl = f'Sim {idx}: agents={n}, intv_cov={intv_cov}, seed={rand_seed}, net={G.name}, rng={rng}'
+    lbl = f'Sim {idx}: agents={n_agents}, cov={cov}, seed={rand_seed}, net={G.name}, rng={rng}'
     print('Starting', lbl)
 
     networks = ss.StaticNet(G)
 
     sir_pars = {
-        'beta': 30,
+        'beta': 75,
         'dur_inf': sps.weibull_min(c=1, scale=30/365), # When c=1, it's an exponential
         #'dur_inf': sps.weibull_min(c=3, scale=33.5/365), # Can check sir_pars['dur_inf'].mean()
         'init_prev': 0,  # Will seed manually
@@ -72,14 +69,16 @@ def run_sim(n, idx, intv_cov, rand_seed, rng, network=None):
         'slot_scale': 10 # Increase slot scale to reduce repeated slots
     }
 
-    if intv_cov > 0:
-        # Create the product - a vaccine with 80% efficacy
-        MyVaccine = ss.sir_vaccine(pars=dict(efficacy=0.8))
+    if cov > 0:
+        # Create the product - a vaccine with specified efficacy
+        MyVaccine = ss.sir_vaccine(pars=dict(efficacy=eff))
 
         # Create the intervention
         MyIntervention = ss.campaign_vx(
-            years = 2020.1, #[pars['start']],
-            prob = intv_cov,
+            #years = [pars['start'], pars['start'] + 4/365, pars['start'] + 5/365], # 5 days after start
+            #prob = [0, 0, cov],
+            years = pars['start'] + 5/365, # 5 days after start
+            prob = cov,
             annual_prob = False,
             product=MyVaccine
         )
@@ -90,76 +89,139 @@ def run_sim(n, idx, intv_cov, rand_seed, rng, network=None):
 
     # Infect agent zero to start the simulation
     # WARNING: Graph algorithms may place agent 0 non-randomly
-    sim.diseases.sir.set_prognoses(sim, uids=np.array([0]), source_uids=None)
+    if fixed_initial_prevalence:
+        uids = np.arange(n_agents//10)
+    else:
+        uids = np.array([0])
+    sim.diseases.sir.set_prognoses(sim, uids=uids, source_uids=None)
 
     sim.run()
 
     df = pd.DataFrame( {
         'year': sim.yearvec,
-        'sir.n_susceptible': sim.results.sir.n_susceptible,
-        'sir.n_infected': sim.results.sir.n_infected,
-        'sir.n_recovered': sim.results.sir.n_recovered,
+        'Susceptible': sim.results.sir.n_susceptible,
+        'Infected': sim.results.sir.n_infected,
+        'Recovered': sim.results.sir.n_recovered,
     })
-    df['intv_cov'] = intv_cov
+    df['cov'] = cov
     df['rand_seed'] = rand_seed
     df['network'] = G.name
+    df['eff'] = eff
     df['rng'] = rng
+    df['n_agents'] = n_agents
 
-    if rand_seed == 0: # Will repead for coverage sweep, but that's okay!
+    ''' Nice plot, but doesn't belong here (figdir not defined either)
+    if debug and rand_seed == 0: # Will repeat for coverage sweep, but that's okay!
         fig = plot_graph(G)
-        fig.savefig(os.path.join(figdir, f'graph_{n}_{G.name}.png'), bbox_inches='tight', dpi=300)
+        fig.savefig(os.path.join(figdir, f'graph_{n_agents}_{G.name}.png'), bbox_inches='tight', dpi=300)
         plt.close(fig)
         print(f'Mean duration of infection is {sir_pars["dur_inf"].mean() * 365} days')
+    '''
 
     print('Finishing', lbl)
 
     return df
 
-def sweep_cov():
+def sweep_cov(n_agents=default_n_agents, n_seeds=default_n_rand_seeds):
+    figdir = os.path.join(basedir, 'SIR_sweep_cov')
+    sc.path(figdir).mkdir(parents=True, exist_ok=True)
+
+    #cov_levels = [0.01, 0.10, 0.25, 0.90] + [0] # Must include 0 as that's the reference level
+    cov_levels = [0.05, 0.80] + [0] # Must include 0 as that's the reference level
+
     results = []
     times = {}
     for rng in rngs:
         ss.options(rng=rng)
         cfgs = []
-        for rs in range(n_rand_seeds):
-            for intv_cov in intv_cov_levels:
-                cfgs.append({'intv_cov':intv_cov, 'rand_seed':rs, 'rng':rng, 'idx':len(cfgs)})
+        for rs in range(n_seeds):
+            G = None
+            #G = nx.connected_watts_strogatz_graph(n=n_agents, k=6, p=0.2, seed=rs) # Small world
+            #G.name = 'Watts-Strogatz'
+    
+            for cov in cov_levels:
+                cfgs.append({'cov':cov, 'rand_seed':rs, 'rng':rng, 'idx':len(cfgs)})
         T = sc.tic()
-        results += sc.parallelize(run_sim, kwargs={'n': n}, iterkwargs=cfgs, die=True, serial=False)
+        results += sc.parallelize(run_sim, kwargs={'n_agents': n_agents, 'network':G, 'fixed_initial_prevalence':False}, iterkwargs=cfgs, die=True, serial=False)
         times[f'rng={rng}'] = sc.toc(T, output=True)
 
     print('Timings:', times)
 
     df = pd.concat(results)
     df.to_csv(os.path.join(figdir, 'sweep_cov.csv'))
+
     return df
 
-def sweep_network():
+'''
+def sweep_network(n_agents=default_n_agents, n_seeds=default_n_rand_seeds):
     results = []
     times = {}
 
     for rng in rngs:
         ss.options(rng=rng)
         cfgs = []
-        for rs in range(n_rand_seeds):
+        for rs in range(n_seeds):
             graphs = {
-                'Barabasi-Albert':  nx.barabasi_albert_graph(n=n, m=1, seed=rs),
-                'Erdos-Renyi':      nx.gnp_random_graph(n=n, p=3/n, seed=rs), #G=nx.erdos_renyi_graph(n, p=3/n, seed=rand_seed)
-                'Watts-Strogatz':   nx.watts_strogatz_graph(n=n, k=3, p=0.25, seed=rs), # Small world
-                'Complete':         nx.complete_graph(n=n),
+                'Barabasi-Albert':  nx.barabasi_albert_graph(n=n_agents, m=1, seed=rs),
+                'Erdos-Renyi':      nx.gnp_random_graph(n=n_agents, p=3/n_agents, seed=rs), #G=nx.erdos_renyi_graph(n, p=3/n, seed=rand_seed)
+                'Watts-Strogatz':   nx.watts_strogatz_graph(n=n_agents, k=3, p=0.25, seed=rs), # Small world
+                'Complete':         nx.complete_graph(n=n_agents),
             }
 
             for name, G in graphs.items():
                 G.name = name
                 cfgs.append({'network':G, 'rand_seed':rs, 'rng':rng, 'idx':len(cfgs)})
         T = sc.tic()
-        results += sc.parallelize(run_sim, kwargs={'n': n, 'intv_cov': 0}, iterkwargs=cfgs, die=True, serial=False)
+        results += sc.parallelize(run_sim, kwargs={'n_agents': n_agents, 'cov': 0}, iterkwargs=cfgs, die=True, serial=False)
         times[f'rng={rng}'] = sc.toc(T, output=True)
 
     print('Timings:', times)
 
     df = pd.concat(results)
     df.to_csv(os.path.join(figdir, 'sweep_network.csv'))
+    return df
+'''
+
+def sweep_n(n_seeds=default_n_rand_seeds):
+    figdir = os.path.join(basedir, 'SIR_sweep_n')
+    sc.path(figdir).mkdir(parents=True, exist_ok=True)
+
+    n_agents_levels = [10, 100, 1000]
+    if not debug:
+        n_agents_levels += [10_000]#, 100_000]
+    #cov_levels = [0, 0.15] # Must include 0 as that's the reference level
+    #efficacy = 0.3
+    cov_levels = [0, 0.05, 0.90] # Must include 0 as that's the reference level
+    efficacy = 0.8
+
+    results = []
+    times = {}
+
+    for rng in rngs:
+        ss.options(rng=rng)
+        cfgs = []
+        for n_agents in n_agents_levels:
+            for rs in range(n_seeds):
+                G = None
+                #G = nx.connected_watts_strogatz_graph(n=n_agents, k=6, p=0.2, seed=rs) # Small world
+                #G.name = 'Watts-Strogatz'
+
+                for cov in cov_levels:
+                    cfgs.append({'n_agents':n_agents, 'cov':cov, 'rand_seed':rs, 'network':G, 'rng':rng, 'idx':len(cfgs)})
+        T = sc.tic()
+        results += sc.parallelize(run_sim, kwargs={'eff':efficacy, 'fixed_initial_prevalence':True}, iterkwargs=cfgs, die=True, serial=False)
+        times[f'rng={rng}'] = sc.toc(T, output=True)
+
+    print('Timings:', times)
+
+    df = pd.concat(results)
+
+    # NORMALIZE
+    for col in ['Susceptible', 'Infected', 'Recovered']:
+        df[col] /= df['n_agents']
+
+    df.to_csv(os.path.join(figdir, 'sweep_n.csv'))
+
     return df
 
 
@@ -168,20 +230,26 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--plot', help='plot from a cached CSV file', type=str)
+    parser.add_argument('-n', help='Number of agents', type=int, default=default_n_agents)
+    parser.add_argument('-s', help='Number of seeds', type=int, default=default_n_rand_seeds)
     args = parser.parse_args()
 
     if args.plot:
         print('Reading CSV file', args.plot)
         df = pd.read_csv(args.plot, index_col=0)
-        raise Exception('TODO')
+        #figdir = os.path.dirname(args.plot)
     else:
         print('Running scenarios')
-        #dfc = sweep_cov()
-        dfn = sweep_network()
+        df = sweep_cov(n_agents=args.n, n_seeds=args.s)
+        #df = sweep_n(n_seeds=args.s)
 
-    #plot_scenarios(df, figdir, sweep_var='network', reference_val=???)
+    figdir = os.path.join(basedir, 'SIR_sweep_cov')
+    plot_scenarios(df, figdir, channels=['Susceptible', 'Infected', 'Recovered'], var1='cov', var2='channel')
+
+    #figdir = os.path.join(basedir, 'SIR_sweep_n')
+    #plot_scenarios(df, figdir, channels=['Recovered'], var1='n_agents', var2='cov', slice_year = -1) # slice_year = 2020.05
     
-    #df = run_sim(n, idx=0, intv_cov=0, rand_seed=0, rng='multi')
+    #df = run_sim(n, idx=0, cov=0, rand_seed=0, rng='multi')
     #print(df)
 
     print('Done')
