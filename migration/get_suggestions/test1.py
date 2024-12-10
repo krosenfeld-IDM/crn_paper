52ba30d1df3d23fe30921f2b8e6a5282c88796ec
Katherine Rosenfeld <krosenf@gmail.com>
2024-12-10 14:34:14 +0000

    refactor get_suggestions to update prompt handling and model usage

--- migration/get_suggestions/test1.py
+++ migration/get_suggestions/test1.py
@@ -1,4 +1,5 @@
 import os
+import re
 import json
 import inspect
 import tomllib
@@ -9,14 +10,30 @@ import crn_paper
 import starsim as ss
 import traceback
 import subprocess
+import tiktoken
 
 os.chdir(os.path.dirname(__file__))
 
-MODEL = 'gpt-4o-mini'
+MODEL = 'gpt-4o'
 
-prompt_str = """
-NOTHING YET
-"""
+prompt_template = '''
+Here is the diff information for an update to the starsim (ss) package:
+------
+{}
+------
+Please update the code below to maintain compatibility with the starsim (ss) code:
+------
+{}
+------
+Return ONLY the updated code:
+'''
+
+def num_tokens_from_string(string: str, model_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    # encoding = tiktoken.get_encoding(encoding_name)
+    encoding = tiktoken.encoding_for_model(model_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
 
 # gets the git diff of a file between two commits
 def get_diff(file, t: dict, padding: int = 10):
@@ -41,9 +58,11 @@ def get_all_diffs(file, mehtods_list):
             if attr_file not in diffs:
                 diffs[attr_file] = stdout
         except AttributeError:
-            print(f"Attribute {method_str} not found")
+            pass
+            # print(f"Attribute {method_str} not found")
         except Exception as e:
-            print(f"traceback: {traceback.format_exc()}")
+            pass
+            # print(f"traceback: {traceback.format_exc()}")
     return diffs
 
 if __name__ == "__main__":
@@ -56,8 +75,11 @@ if __name__ == "__main__":
     code_dir = Path(t['info']['code']).resolve()
 
     # open the list of methods
-    with open(migration_dir / f'results/identified_ss_{MODEL}.json','r') as f:
+    with open(migration_dir / f'results/identified_ss_gpt-4o-mini.json','r') as f:
         methods_dict = json.load(f)
+
+    # create the query object
+    query = sa.SimpleQuery(model=MODEL)
 
     # looping over the project files and methods found
     for file, methods_list in methods_dict.items():
@@ -68,7 +90,22 @@ if __name__ == "__main__":
         diffs = get_all_diffs(file, methods_list)
         print(f"Number of diffs: {len(diffs)}")
 
+        # load the file
+        code_file = Path(t['info']['code']).resolve() / file
+        with open(code_file, 'r') as f:
+            code_string = f.read()
+
         if len(diffs) > 0:
-            # construct the prompt.
-            pass
+            # construct the prompt
+            prompt = prompt_template.format("\n".join(diffs.values()), code_string)
+            print(f"Number of tokens: {num_tokens_from_string(prompt, MODEL)}")
+            response = query(prompt)
+            pattern = "```python(.*?)```"
+            matches = re.findall(pattern, response.content, re.DOTALL)
+            with open(migration_dir / 'results' / f'{code_file.stem}_{MODEL}.py', 'w') as f:
+                f.write(matches[0])
+            with open(code_file, 'w') as f:
+                f.write(matches[0])
+
+
    
